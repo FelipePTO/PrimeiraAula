62b5edd84049d80918f1c52c230c3c4878affffa
FelipePTO <felipepaulotavares@gmail.com>
2021-04-05 09:17:08 -0300

    Primeira aula

---
+++ DonetCoreTrilha/DonetCoreTrilha/Controllers/TurmaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonetCoreTrilha.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TurmaController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TurmaController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("OI TURMA");
+        }
+    }
+}
